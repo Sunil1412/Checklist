fa1b04689923ae568953dd7f552bff151ba17b5e
SUNIL <86763715+ZEROSOFTWAREDEVELOPER@users.noreply.github.com>
2022-03-11 03:43:03 +0530

    deploy my react app to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state={
+
+    isCitizen:false,
+    isOver21: false
+  }
+
+  onChange= e=>{
+    if(e.target.type==="checkbox")
+    this.setState({[e.target.name]: e.target.checked})
+    else
+    this.setState({[e.target.name] : e.target.value})
+  }
+
+  render() {
+
+    const {isCitizen,isOver21} = this.state
+
+    return (
+      <div>
+        <form>
+
+          <h1>Are You Citizen : {isCitizen ? "Yes" : "No"}</h1>
+          <h1>Are You over 21  : {  isOver21 ? "Yes" : "No"}</h1>
+
+          <label>
+            Are You Citizen?
+
+            <input type="checkbox"
+            checked={isCitizen}
+            name='isCitizen'
+            onChange={this.onChange}/>
+          </label>
+
+          <label>
+           Are You over 21?
+
+            <input type="checkbox"
+            checked={  isOver21}
+            name='isOver21'
+            onChange={this.onChange}/>
+          </label>
+          
+         
+        </form>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
